bfcc9ea3c7cf6c615c6ab611ce0cce6a5823adb8
benmaia <godlinhomaia@gmail.com>
2022-07-07 18:43:23 +0100

    duplicates + special parsing

--- minishell/srcs/export.c
+++ minishell/srcs/export.c
@@ -6,7 +6,7 @@
 /*   By: bmiguel- <bmiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 00:29:06 by bmiguel-          #+#    #+#             */
-/*   Updated: 2022/07/06 05:51:47 by bmiguel-         ###   ########.fr       */
+/*   Updated: 2022/07/07 18:41:31 by bmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,56 @@ void	check_spaces(t_data *d, int i)
 	free (tmp);
 }
 
+int	checker(t_data *d, int i)
+{
+	while (d->buf[++i])
+	{
+		if (d->buf[i] == ' ')
+		{
+			perror("error");
+			return (0);
+			/*exit(1);*/
+		}
+		else if (d->buf[i] == '=')
+			return (1);
+	}
+	if (d->buf[i] == '\0')
+	{
+		perror("error2");
+		/*exit(1);*/
+	}
+	return (0);
+}
+
+int	check_duplicates(t_data *d)
+{
+	t_list	*tmp;
+	char	*var;
+	int		i;
+
+	i = -1;
+	while (d->buf[++i])
+	{
+		if (d->buf[i] == '=')
+			break ;
+	}
+	var = ft_substr(d->buf, 0, i);
+	tmp = d->env;
+	while (tmp)
+	{
+		if (!ft_strncmp(tmp->content, var, ft_strlen(var)))
+		{
+			free (var);
+			free (tmp->content);
+			tmp->content = ft_strdup(d->buf);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	free (var);
+	return (0);
+}
+
 void	ft_export(t_data *d)
 {
 	t_list	*new_var;
@@ -68,8 +118,12 @@ void	ft_export(t_data *d)
 	else
 	{
 		check_spaces(d, -1);
-		new_var = ft_lstnew(d->buf);
-		ft_lst_strprint(new_var);
-		ft_lstadd_back(&d->env, new_var);
+		if (checker(d, -1))
+		{
+			if (check_duplicates(d))
+				return ;
+			new_var = ft_lstnew(ft_strdup(d->buf));
+			ft_lstadd_back(&d->env, new_var);
+		}
 	}
 }
