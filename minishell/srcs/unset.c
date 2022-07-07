012ec1eb00c604e6e8b2dab2e5cefe7f7f5d75b4
benmaia <godlinhomaia@gmail.com>
2022-07-07 22:01:24 +0100

    unset compare

--- minishell/srcs/unset.c
+++ minishell/srcs/unset.c
@@ -6,12 +6,45 @@
 /*   By: bmiguel- <bmiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:57:06 by bmiguel-          #+#    #+#             */
-/*   Updated: 2022/07/07 17:33:49 by bmiguel-         ###   ########.fr       */
+/*   Updated: 2022/07/07 20:40:52 by bmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 #include <stdio.h>
+
+int	check_size(int x, int z)
+{
+	if (x > z)
+		return (x);
+	else
+		return (z);
+}
+
+int	content_size(char *content, int i)
+{
+	while (content[++i])
+	{
+		if (content[i] == '=')
+			return (i);
+	}
+	return (i);
+}
+
+void	check_head(t_data *d, char *var, t_list **tmp, t_list **next)
+{
+	t_list	**head;
+
+	head = &d->env;
+	if (!ft_strncmp((*head)->content, var, ft_strlen(var)))
+	{
+		*next = *head;
+		*head = (*head)->next;
+		*tmp = (*tmp)->next;
+		free ((*next)->content);
+		free (*next);
+	}
+}
 
 void	check_var(t_data *d, char *var)
 {
@@ -19,9 +52,13 @@ void	check_var(t_data *d, char *var)
 	t_list	*next;
 
 	tmp = d->env;
-	while (tmp)
+	next = NULL;
+	check_head(d, var, &tmp, &next);
+	while (tmp->next)
 	{
-		if (!ft_strncmp(tmp->next->content, var, ft_strlen(var)))
+		if (!ft_strncmp(tmp->next->content, var,
+				check_size(ft_strlen(var),
+					content_size(tmp->next->content, -1))))
 		{
 			next = tmp->next;
 			tmp->next = tmp->next->next;
