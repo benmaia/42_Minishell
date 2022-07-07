bf110fbb6fa27924617018194398f34fd958a4f9
benmaia <godlinhomaia@gmail.com>
2022-07-08 00:06:18 +0100

    parsing readline to struct

--- minishell/srcs/parser.c
+++ minishell/srcs/parser.c
@@ -5,47 +5,49 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmiguel- <bmiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/03 11:28:56 by bmiguel-          #+#    #+#             */
-/*   Updated: 2022/07/07 16:40:03 by bmiguel-         ###   ########.fr       */
+/*   Created: 2022/07/07 22:01:59 by bmiguel-          #+#    #+#             */
+/*   Updated: 2022/07/08 00:00:23 by bmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-#include <readline/readline.h>
 #include <stdio.h>
 
-int main (int argc, char **argv, char **envp)
+int	doublee(t_data *d, int i)
 {
-	t_data d;
+	i++;
+	while (d->buf[i] != '\"')
+		i++;
+	return (i);
+}
 
-	(void) argc;
-	(void) argv;
-	d.env = init_env(envp);
-	while (1)
+int	simple(t_data *d, int i)
+{
+	i++;
+	while (d->buf[i] != '\'')
+		i++;
+	return (i);
+}
+
+t_promp	*parser_promp(t_data *d, int i)
+{
+	t_promp	*promp;
+	int		j;
+
+	promp = NULL;
+	j = 0;
+	while (d->buf[++i])
 	{
-		d.buf = readline (RED"MINISHELL > "RES);
-		if (!ft_strncmp(d.buf, "export", 6))
-			ft_export(&d);
-		quotes(&d, -1);
-		if (!ft_strncmp(d.buf, "unset", 5))
-			ft_unset(&d);
-		if (!ft_strncmp(d.buf, "pwd", 3))
-			ft_pwd();
-		if (!ft_strncmp(d.buf, "env", 3))
-			ft_env(&d);
-		if (!ft_strncmp(d.buf, "cd", 2))
-			cd(&d);
-		if (!ft_strncmp(d.buf, "echo", 4))
-			ft_echo(&d);
-		if (!ft_strncmp(d.buf, "exit\0", 5))
+		if (d->buf[i] == '\"')
+			i = doublee(d, i);
+		else if (d->buf[i] == '\'')
+			i = simple(d, i);
+		else if (d->buf[i] == '|')
 		{
-			free (d.buf);
-			ft_free_stack(&d.env);
-			exit (1);
+			ft_prompadd(&promp, ft_prompnew(ft_substr(d->buf, j, i - j)));
+			j = i + 1;
 		}
-		free (d.buf);
 	}
-	/*ft_free_stack(&d.env);*/
-	/*ft_free_stack(&d.env);*/
-	return (0);
+	ft_prompadd(&promp, ft_prompnew(ft_substr(d->buf, j, i - j)));
+	return (promp);
 }
